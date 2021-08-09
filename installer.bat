7bd27dcad34f833bbf934a598fd19969ec9e707d
f0e <7321764+f0e@users.noreply.github.com>
2021-08-09 21:31:33 +1000

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -51,7 +51,7 @@ echo.
 
 :: Install Chocolatey
 echo Installing Chocolatey...
-powershell Set-ExecutionPoicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 pause
 
