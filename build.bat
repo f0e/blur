ed0cf7f98c3096501c1d931a99d4183f4909d12d
Couleur <82747632+couleurm@users.noreply.github.com>
2022-01-30 19:28:36 +0100

    Remove old overcomplicated code that
    New code is very simplified and fits in a single py file
    
    No hate to Aetopia for anyone wondering

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-py -OO -m nuitka --onefile --standalone --follow-imports --include-plugin-files=scripts\config.py --include-plugin-files=scripts\encoder.py --include-plugin-files=scripts\vs_script.py --include-plugin-files=scripts\render.py main.py --output-dir="%TEMP%" -o "Smoothie.exe"
-timeout 5
