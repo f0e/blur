db2847107573185da63424b560a0ad2d7f2f1a5e
Couleur <82747632+couleurm@users.noreply.github.com>
2022-02-04 22:08:51 +0100

    replace smoothie shim by sm, add warnings

--- install/post.ps1
+++ install/post.ps1
@@ -1,25 +1,26 @@
 Rename-Item -Path "$DIR\Smoothie*" -NewName "Smoothie"
 
-if (-Not(Test-Path "$ScoopDir\shims\Smoothie.exe")){
-    Copy-Item "$ScoopDir\shims\7z.exe" "$ScoopDir\shims\Smoothie.exe" # All shims are the same
+if (-Not(Test-Path "$ScoopDir\shims\sm.exe")){
+    Copy-Item "$ScoopDir\shims\7z.exe" "$ScoopDir\shims\sm.exe" # All shims are the same
 }
-if (-Not(Test-Path "$ScoopDir\shims\Smoothie.shim")){
+if (-Not(Test-Path "$ScoopDir\shims\sm.shim")){
 
-    New-Item "$ScoopDir\shims\Smoothie.shim" -Force
-    Add-Content "$ScoopDir\shims\Smoothie.shim" "path = `"$DIR\python.exe`"$([Environment]::NewLine)args = `"$DIR\Smoothie\Smoothie.py`""
+    New-Item "$ScoopDir\shims\sm.shim" -Force
+    Add-Content "$ScoopDir\shims\sm.shim" "path = `"$DIR\python.exe`"$([Environment]::NewLine)args = `"$DIR\Smoothie\Smoothie.py`""
 }
 
-# havsfunc, mvsfunc, filldrops & adjust 
+Write-Warning "Importing havsfunc,mvsfunc, filldrops & adjust"
 Move-Item "$DIR\Smoothie\*.py" "$DIR\vapoursynth64\plugins"
 
-# vs-frameblender
+Write-Warning "Downloading vs-frameblender"
 Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/f0e/vs-frameblender/releases/latest/download/vs-frameblender-x64.dll' -OutFile "$DIR\vapoursynth64\plugins\vs-frameblender-x64.dll"
 
-# ffms2 & libmv
+Write-Warning "Updating vsrepo"
 & "$DIR\python.exe" "$DIR\vsrepo.py" update
+Write-Warning "Installing ffms2 & libmvtools"
 & "$DIR\python.exe" "$DIR\vsrepo.py" install ffms2 mv
 
-# SVPFlow
+Write-Warning "Downloading, extracting & installing SVPFlow"
 Remove-Item "$env:TMP\SVPFlow.7z","$env:TMP\SVPFlow" -Force -Ea Ignore -Recurse
 Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/bjaan/smoothvideo/main/SVPflow_LastGoodVersions.7z' -OutFile "$env:TMP\SVPFlow.7z"
 while (-not(Test-Path "$env:TMP\SVPFlow.7z")){$null}
