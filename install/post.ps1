d144e434654a714f2ef6358ed5a2d1e8985c880c
Couleur <82747632+couleurm@users.noreply.github.com>
2022-02-08 20:41:02 +0100

    update path, remove plugins install & note about nvenc

--- install/post.ps1
+++ install/post.ps1
@@ -6,29 +6,10 @@ if (-Not(Test-Path "$ScoopDir\shims\sm.exe")){
 if (-Not(Test-Path "$ScoopDir\shims\sm.shim")){
 
     New-Item "$ScoopDir\shims\sm.shim" -Force
-    Add-Content "$ScoopDir\shims\sm.shim" "path = `"$DIR\python.exe`"$([Environment]::NewLine)args = `"$DIR\Smoothie\Smoothie.py`""
+    Add-Content "$ScoopDir\shims\sm.shim" "path = `"$DIR\VapourSynth\python.exe`"$([Environment]::NewLine)args = `"$DIR\Smoothie\Smoothie.py`""
 }
 
-Write-Warning "Importing havsfunc,mvsfunc, filldrops & adjust"
-Move-Item "$DIR\Smoothie\plugins\*.py" "$DIR"
-
-Write-Warning "Downloading vs-frameblender"
-Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/f0e/vs-frameblender/releases/latest/download/vs-frameblender-x64.dll' -OutFile "$DIR\vapoursynth64\plugins\vs-frameblender-x64.dll"
-
-Write-Warning "Updating vsrepo"
-& "$DIR\python.exe" "$DIR\vsrepo.py" update
-Write-Warning "Installing ffms2 & libmvtools"
-& "$DIR\python.exe" "$DIR\vsrepo.py" install ffms2 mv
-
-Write-Warning "Downloading, extracting & installing SVPFlow"
-Remove-Item "$env:TMP\SVPFlow.7z","$env:TMP\SVPFlow" -Force -Ea Ignore -Recurse
-Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/bjaan/smoothvideo/main/SVPflow_LastGoodVersions.7z' -OutFile "$env:TMP\SVPFlow.7z"
-while (-not(Test-Path "$env:TMP\SVPFlow.7z")){$null}
-7z x  "$env:TMP\SVPFlow.7z" -o"$env:TMP\SVPFlow"
-Move-Item "$env:TEMP\svpflow\x64_vs\*.dll" "$DIR\vapoursynth64\plugins" -Force
-Remove-Item "$env:TMP\SVPFlow.7z","$env:TMP\SVPFlow" -Force -Ea Ignore -Recurse
-
-Write-Warning "Testing FFmpeg encoders.."
+Write-Warning "Testing FFmpeg encoders.. note NVENC may fail if you use an older driver / older FFmpeg version"
 @(
     'hevc_nvenc -rc constqp -preset p7 -qp 18'
     'h264_nvenc -rc constqp -preset p7 -qp 15'
