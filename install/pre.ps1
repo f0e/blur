eeb92be3e5ee60061710ef8bf83c8b34b052159d
Couleur <82747632+couleurm@users.noreply.github.com>
2022-03-26 14:56:09 +0100

    lots of fixes and speed improvements
    using scoop(.ps1) instead of scoop.cmd
    fixed utils bucket check
    fixed scoop install as admin
    more comprehensive process
    fixed switch

--- install/pre.ps1
+++ install/pre.ps1
@@ -1,54 +1,72 @@
-if (Get-Command sm  -Ea Ignore){
-@"
+Set-ExecutionPolicy Bypass -Scope Process -Force # Required for scoop
+
+function CheckProcesses{
+    if (Get-Process -Name python,sm,vspipe,ffmpeg -ErrorAction Ignore){
+        Write-Warning @"
+The following processes are still running and may cause uninstallation issues
+
+make sure these processes are not directly related to Smoothie before continuing:
+"@
+        Get-Process -Name python,sm,vspipe,ffmpeg -ErrorAction Ignore | Select-Object ProcessName, Path
+        pause
+    }     
+
+}
+
+if (Get-Command sm -Ea Ignore){
+    Write-Output @"
 
 A Smoothie installation has been detected, what would you like to do?
 
 Press R to Reinstall Smoothie (clean install)
 Press V to update Smoothie to a newer version
 Press U to uninstall Smoothie
+Press Q to quit
 "@
-
-function CheckProcesses{
-    if (Get-Process -Name sm,vspipe,ffmpeg -ErrorAction SilentlyContinue){
-        Write-Warning "One of the following processes are still running and may cause uninstallation issues, make sure these processes are not tied to Smoothie:"
-        Get-Process -Name sm,vspipe,ffmpeg -ErrorAction SilentlyContinue
-    }     
-
-}
-switch (choice.exe /C RVU /N | Out-Null){
-    1{
-        CheckProcesses
-        scoop.cmd uninstall smoothie
-        scoop.cmd install smoothie
-        pause
-        exit
-    }
-    2{
-        CheckProcesses
-        scoop.cmd update smoothie
-        pause
-        exit
-    }
-    3{
-        CheckProcesses
-        scoop.cmd uninstall smoothie
-        pause
-        exit
+    $Choice = choice.exe /C RVUQ /N
+    switch ($Choice){
+        'R'{
+            CheckProcesses
+            scoop uninstall smoothie
+            scoop install smoothie
+            pause
+            exit
+        }
+        'V'{
+            CheckProcesses
+            scoop update smoothie
+            pause
+            exit
+        }
+        'U'{
+            CheckProcesses
+            scoop uninstall smoothie
+            pause
+            exit
+        }
+        'Q'{
+            exit
+        }
     }
 }
-}
 
-if (-Not(Get-Command scoop.cmd -Ea Ignore)){
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    Invoke-RestMethod -Uri http://get.scoop.sh | Invoke-Expression
+if (-Not(Get-Command scoop -Ea Ignore)){
+
+    $RunningAsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')
+
+    If (-Not($RunningAsAdmin)){
+        Invoke-Expression (Invoke-WebRequest -UseBasicParsing -Uri http://get.scoop.sh)
+    }else{
+        Invoke-Expression "& {$(Invoke-WebRequest -UseBasicParsing -Uri https://get.scoop.sh)} -RunAsAdmin"
+    }
 }
 
 if (-Not(Get-Command git -Ea Ignore)){
-    scoop.cmd install git
+    scoop install git
 }
 
-if ('utils' -NotIn (scoop.cmd bucket list)){
-    scoop.cmd bucket add utils https://github.com/couleur-tweak-tips/utils
+if ('utils' -NotIn ((scoop bucket list).Name)){
+    scoop bucket add utils https://github.com/couleur-tweak-tips/utils
 }
 
 $IsFFmpegScoop = (Get-Command ffmpeg -Ea Ignore).Source -Like "*\shims\*"
@@ -60,13 +78,13 @@ if(Get-Command ffmpeg -Ea Ignore){
     if (-Not($IsFFmpeg5)){
 
         if ($IsFFmpegScoop){
-            scoop.cmd update ffmpeg
+            scoop update ffmpeg
         }else{
             Write-Warning @"
 An FFmpeg installation was detected, but it is not version 5.0 or higher.
 If you installed FFmpeg yourself, you can remove it and use the following command to install ffmpeg and add it to the path:
 
-scoop.cmd install ffmpeg
+scoop install ffmpeg
 "@
             
         }
@@ -74,13 +92,13 @@ scoop.cmd install ffmpeg
     }
             
 }else{
-    scoop.cmd install ffmpeg
+    scoop install ffmpeg
 }
 
-if ('utils' -NotIn (scoop.cmd bucket list)){
+if ('utils' -NotIn (scoop bucket list).Name){
     Write-Warning "Failed to add the utils bucket"
     pause
     exit
 }
 
-scoop.cmd install utils/smoothie
+scoop install utils/smoothie
