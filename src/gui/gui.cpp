7fa54874abef7b9c8bbea3444785e4c6c2f27224
f0e <7321764+f0e@users.noreply.github.com>
2024-11-24 06:54:52 +1000

    feat: vsync in event when actively rendering

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -20,6 +20,7 @@ const float fps_smoothing = 0.95f;
 const float vsync_extra_fps = 50;
 const float min_delta_time = 1.f / 10;
 const float default_delta_time = 1.f / 60;
+float vsync_frame_time = default_delta_time;
 
 bool closing = false;
 bool actively_rendering = true;
@@ -111,33 +112,39 @@ void gui::generate_messages_from_os_events() { // https://github.com/aseprite/as
 
 void gui::event_loop() {
 	while (!closing) {
-		redraw_window(window.get());
+#ifdef _WIN32
+		HMONITOR screen_handle = (HMONITOR)window->screen()->nativeHandle();
+		static HMONITOR last_screen_handle;
+#elif defined(__APPLE__)
+		void* screen_handle = window->screen()->nativeHandle();
+		static void* last_screen_handle;
+#else
+		int screen_handle = (int)window->screen()->nativeHandle();
+		static int last_screen_handle;
+#endif
 
+		if (screen_handle != last_screen_handle) {
+			double rate = utils::get_display_refresh_rate(screen_handle);
+			vsync_frame_time = 1.f / (rate + vsync_extra_fps);
+			printf("switched screen, updated vsync_frame_time. refresh rate: %.2f hz\n", rate);
+			last_screen_handle = screen_handle;
+		}
+
+		auto frame_start = std::chrono::steady_clock::now();
+
+		redraw_window(window.get());
 		generate_messages_from_os_events();
+
+		if (actively_rendering) {
+			auto target_time = frame_start + std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+												 std::chrono::duration<float>(vsync_frame_time)
+											 );
+			std::this_thread::sleep_until(target_time);
+		}
 	}
 }
 
 void gui::redraw_window(os::Window* window) {
-	static float vsync_frame_time = default_delta_time;
-
-#ifdef _WIN32
-	HMONITOR screen_handle = (HMONITOR)window->screen()->nativeHandle();
-	static HMONITOR last_screen_handle;
-#elif defined(__APPLE__)
-	void* screen_handle = window->screen()->nativeHandle();
-	static void* last_screen_handle;
-#else
-	int screen_handle = (int)window->screen()->nativeHandle();
-	static int last_screen_handle;
-#endif
-
-	if (screen_handle != last_screen_handle) {
-		double rate = utils::get_display_refresh_rate(screen_handle);
-		vsync_frame_time = 1.f / (rate + vsync_extra_fps);
-		printf("switched screen, updated vsync_frame_time. refresh rate: %.2f hz\n", rate);
-		last_screen_handle = screen_handle;
-	}
-
 	auto now = std::chrono::steady_clock::now();
 	static auto last_frame_time = now;
 
@@ -152,14 +159,10 @@ void gui::redraw_window(os::Window* window) {
 	else {
 		float time_since_last_frame = std::chrono::duration<float>(std::chrono::steady_clock::now() - last_frame_time).count();
 
-		if (time_since_last_frame < vsync_frame_time) {
-			return;
-		}
-
-		float current_fps = 1.f / time_since_last_frame;
-		if (fps == -1.f)
-			fps = current_fps;
-		fps = (fps * fps_smoothing) + (current_fps * (1.0f - fps_smoothing));
+		float fps = 1.f / time_since_last_frame;
+		// if (fps == -1.f)
+		// 	fps = current_fps;
+		// fps = (fps * fps_smoothing) + (current_fps * (1.0f - fps_smoothing));
 
 		delta_time = std::min(time_since_last_frame, min_delta_time);
 	}
