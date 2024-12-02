17693903df23a715365b6d955af2ac9a0557bbe2
f0e <7321764+f0e@users.noreply.github.com>
2024-12-02 11:49:18 +1000

    refactor: clean up gui file argument parsing

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -9,19 +9,7 @@ int app_main(int argc, char* argv[]) { // NOLINT
 
 	std::vector<std::string> arguments(argv + 1, argv + argc);
 
-	// redundant since add_files checks paths too so i could just pass arguments to it, but id rather keep the logic
-	// here for the clarity
-	std::vector<std::string> passed_file_paths;
-	for (const auto& file_path_str : arguments) {
-		std::filesystem::path file_path(file_path_str);
-
-		if (!file_path.empty() && std::filesystem::exists(file_path))
-			passed_file_paths.push_back(file_path_str);
-	}
-
-	std::thread(tasks::run).detach();
-
-	tasks::add_files(passed_file_paths);
+	std::thread(tasks::run, arguments).detach();
 
 	gui::run();
 
