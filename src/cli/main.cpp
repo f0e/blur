ec74fbac6e768c95ca4c1fb5a1903fc6eb14e056
f0e <7321764+f0e@users.noreply.github.com>
2024-12-04 14:10:50 +1000

    feat: copy stuff on build, fix macos exception compile issue

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -15,15 +15,9 @@ int main(int argc, char* argv[]) {
 	app.add_flag("-p,--preview", preview, "Enable preview");
 	app.add_flag("-v,--verbose", verbose, "Verbose mode");
 
-	try {
-		CLI11_PARSE(app, argc, argv);
+	CLI11_PARSE(app, argc, argv);
 
-		cli::run(inputs, outputs, config_paths, preview, verbose);
+	cli::run(inputs, outputs, config_paths, preview, verbose);
 
-		return 0;
-	}
-	catch (const CLI::ParseError& e) {
-		u::log("Failed to parse arguments, use -h or --help for help."); // note: should this be log error?
-		return 1;
-	}
+	return 0;
 }
